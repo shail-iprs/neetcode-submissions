1dc2c763b8a3fdf1d5bc0c795a94987425447886
shail-iprs <shail.iprs@gmail.com>
2026-04-09 23:11:23 +0530

    Add: surrounded-regions - submission-5

---
+++ Data Structures & Algorithms/surrounded-regions/submission-5.py
@@ -0,0 +1,29 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        q=deque()
+        inds=[[0,1],[0,-1],[-1,0],[1,0]]
+
+        m=len(board)
+        n=len(board[0])
+
+        for r in range(m):
+            for c in range(n):
+                if (r in [0,m-1] or c in [0,n-1]) and board[r][c]=='O':
+                    board[r][c]='T'
+                    q.append((r,c))
+
+        while q:
+            r,c=q.popleft()
+            for item in inds:
+                nr,nc=item[0]+r,item[1]+c
+                if 0<=nr<m and 0<=nc<n and board[nr][nc]=='O':
+                    board[nr][nc]='T'
+                    q.append((nr,nc))
+
+        for r in range(m):
+            for c in range(n):
+                if board[r][c]=='O':
+                    board[r][c]='X'
+                if board[r][c]=='T':
+                    board[r][c]='O'
+            
