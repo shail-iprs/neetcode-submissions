f4e3427cff7d56c3b8474af60104be8198211b5c
shail-iprs <shail.iprs@gmail.com>
2026-04-26 15:37:30 +0530

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        buy=prices[0]
+        mx_profit=0
+        for ind in range(1,len(prices)):
+            buy=min(buy,prices[ind])
+            profit=prices[ind]-buy
+            mx_profit=max(mx_profit,profit)
+        return mx_profit
